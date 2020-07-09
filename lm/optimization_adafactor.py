f72284b59fb34de23eddde1c708249d253b4642f
Crates <crates@users.noreply.github.com>
2020-07-09 12:10:18 -0400

    Fixed compatibility issue for Grover

--- lm/optimization_adafactor.py
+++ lm/optimization_adafactor.py
@@ -19,12 +19,12 @@ from lm.utils import get_shape_list
 
 def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
     """Creates an optimizer training op."""
-    global_step = tf.train.get_or_create_global_step()
+    global_step = tf.compat.v1.train.get_or_create_global_step()
 
     learning_rate = tf.constant(value=init_lr, shape=[], dtype=tf.float32)
 
     # Implements linear decay of the learning rate.
-    learning_rate = tf.train.polynomial_decay(
+    learning_rate = tf.compat.v1.train.polynomial_decay(
         learning_rate,
         global_step,
         num_train_steps,
@@ -85,7 +85,7 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
     return train_op, train_metrics
 
 
-class AdaFactorOptimizer(tf.train.Optimizer):
+class AdaFactorOptimizer(tf.compat.v1.train.Optimizer):
     """here's the optimizer we'll use"""
 
     def __init__(self,
